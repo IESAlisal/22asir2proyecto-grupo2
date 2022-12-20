30b6968011b1e3e860bbdd442f774ce775de65f8
vielkafernandez <97469801+vielkafernandez@users.noreply.github.com>
2022-12-20 17:08:07 +0100

    cambio

--- code-libro.php
+++ code-libro.php
@@ -15,4 +15,25 @@ $resultado->close();
 $conexion->close();
 return $libros;
 }
+
+
+function getlibrostitulo()
+{
+	$conexion=getConexionMySQLi();
+	$consulta="select titulo from libros";
+	$libros=[];
+if ($resutlado=$conexion->query($consulta);
+{
+	while($libro=$resultado->fetch_object())
+{
+	$libros[]=$libro->titulo;
+}
+$resultado->close();
+}	
+$conexion->close();
+echo $libros;
+return $libros;
+}
+
+
 ?>
